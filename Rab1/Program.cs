4217294e19ae4e495022bca01aaff23c25048fa6
RusGol718 <ruslanf743@gmail.com>
2023-01-24 19:21:05 +0300

    Solution

---
+++ Rab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using GeometryLibrary;
+namespace Rab1
+{
+    class Program
+    {
+        public static unsafe void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста посмотрите решения задания 2 в Geometry.cs");
+            Console.WriteLine("Пожалуйста посмотрите теcты к заданию 2 в GeometryTests.cs");
+            Console.WriteLine("Пожалуйста посмотрите решения задания 3 в product-task.sql");
+        }
+        
+        
+    }
+}
